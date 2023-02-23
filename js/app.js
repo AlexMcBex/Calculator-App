03c29b573d2d8c670505d2629aab48adb772b7ac
AlexMcBex <alessiobelli94@gmail.com>
2023-02-23 09:56:57 -0500

    linked in html, pseudo code in js

---
+++ js/app.js
@@ -0,0 +1,29 @@
+///// IMPORT ELEMENTS FROM HTML////////
+//import calculator shell
+//import calc screen
+//import number buttons
+//import operation buttons
+    //delete button
+    // plus 
+    //minus
+    //multiply
+    //divide
+    //reset
+    //equal
+    //decimal .
+
+/////DEFINE VARIABLES////////////
+    //calc text box
+
+////// DEFINE FUNCTIONS /////////////
+    //push values in screen (key pressed)
+    //reset -> empty screen
+    //equal -> 
+        //change format in math operation
+        //solve it
+        //push value in screen
+    //delete -> pop last character from screen
+
+
+/////// EVENTS ///////
+//keys.onclick
